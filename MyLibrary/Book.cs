6568b5a95e4cd2cedb60234265e9e9dc4b246bc7
Carlos Loitti <carlosloitir@gmail.com>
2024-10-10 12:08:51 -0500

    Clase Book

--- MyLibrary/Book.cs
+++ MyLibrary/Book.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace MyLibrary
 {
-    public class Book
+    public class Book : Product
     {
+        public string Title { get; }
+
+        public Book (int id, string name, decimal unitPrice, int unitsInStock, string title) :
+            base (id, name, unitPrice, unitsInStock)
+        {
+
+            Title = title;
+        }       
+
     }
+    public class Book2(int id, string name, decimal unitPrice, int unitsInStock, string title) :
+            Product(id, name, unitPrice, unitsInStock)
+    {
+
+        public string Title => title;
+    }
+
 }
