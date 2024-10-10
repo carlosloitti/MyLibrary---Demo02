389ad7319138ddc63185691707de55424bbcb644
Carlos Loitti <carlosloitir@gmail.com>
2024-10-10 11:58:03 -0500

    Class Product

---
+++ MyLibrary/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class Product(int id, string name, decimal unitPrice, int unitsInStock)
+    {
+        public int Id => id;
+        public string Name => name;
+        public decimal UniPrice => unitPrice;
+        public int UnitsInStock => unitsInStock;
+
+        public bool Discontinued {  get; private set; }
+
+        public void SetDiscontinued () =>
+            Discontinued = true;
+
+        public bool GetStatus() =>
+        Discontinued;
+
+
+    }
+}
